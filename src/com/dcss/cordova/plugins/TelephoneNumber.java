1ee6aec110ed96d6c99f9d7cbef1ed646ee76350
Diego Cassinera <diegoc@olx.com>
2015-02-01 18:36:57 -0800

    df

--- src/com/dcss/cordova/plugins/TelephoneNumber.java
+++ src/com/dcss/cordova/plugins/TelephoneNumber.java
@@ -5,6 +5,7 @@ import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.json.JSONException;
 import android.content.Context;
 import android.telephony.TelephonyManager;
 
@@ -24,15 +25,16 @@ public class TelephoneNumber extends CordovaPlugin {
                 (TelephonyManager)this.cordova.getActivity().getSystemService(Context.TELEPHONY_SERVICE);
                     
           JSONObject result = new JSONObject();
-          try {result.put("Line1Number", telephonyManager.getLine1Number());} catch(Exception e) {result.put("excp", e);}
-          try {result.put("VoiceMailNumber", telephonyManager.getVoiceMailNumber());} catch(Exception e) {result.put("excp1", e);}
-          try {result.put("DeviceId", telephonyManager.getDeviceId());} catch(Exception e) {result.put("excp2", e);}
-          try {result.put("SimCountryIso", telephonyManager.getSimCountryIso());} catch(Exception e) {result.put("excp3", e);}
-          try {result.put("SimOperator", telephonyManager.getSimOperator());} catch(Exception e) {result.put("excp4", e);}
-          try {result.put("SimOperatorName", telephonyManager.getSimOperatorName());} catch(Exception e) {result.put("excp5", e);}
-          try {result.put("SimSerialNumber", telephonyManager.getSimSerialNumber());} catch(Exception e) {result.put("excp6", e);}
-          try {result.put("SimState", telephonyManager.getSimState());} catch(Exception e) {result.put("excp7", e);}
-          try {result.put("SubscriberId", telephonyManager.getSubscriberId());} catch(Exception e) {result.put("excp8", e);}
+          
+          try {result.put("Line1Number", telephonyManager.getLine1Number());} catch(JSONException e) {result.put("excp", e);}
+          try {result.put("VoiceMailNumber", telephonyManager.getVoiceMailNumber());} catch(JSONException e) {result.put("excp1", e);}
+          try {result.put("DeviceId", telephonyManager.getDeviceId());} catch(JSONException e) {result.put("excp2", e);}
+          try {result.put("SimCountryIso", telephonyManager.getSimCountryIso());} catch(JSONException e) {result.put("excp3", e);}
+          try {result.put("SimOperator", telephonyManager.getSimOperator());} catch(JSONException e) {result.put("excp4", e);}
+          try {result.put("SimOperatorName", telephonyManager.getSimOperatorName());} catch(JSONException e) {result.put("excp5", e);}
+          try {result.put("SimSerialNumber", telephonyManager.getSimSerialNumber());} catch(JSONException e) {result.put("excp6", e);}
+          try {result.put("SimState", telephonyManager.getSimState());} catch(JSONException e) {result.put("excp7", e);}
+          try {result.put("SubscriberId", telephonyManager.getSubscriberId());} catch(JSONException e) {result.put("excp8", e);}
           callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, result));
              return true;
         }
